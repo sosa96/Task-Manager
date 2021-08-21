b330eae0a8d4efab7dc5fa7e9d03e2155aa2a0be
Zdravko Sosa <zdravko.sosa@gmail.com>
2021-08-21 22:29:19 +0200

    Added endpoints for deleting resources

--- src/index.js
+++ src/index.js
@@ -77,6 +77,23 @@ app.patch('/users/:id', async (req, res) => {
   }
 });
 
+// Delete user
+app.delete('/users/:id', async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const user = await User.findByIdAndDelete(_id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
 // Create task
 app.post('/tasks', async (req, res) => {
   const task = new Task(req.body);
@@ -143,6 +160,23 @@ app.patch('/tasks/:id', async (req, res) => {
     res.send(task);
   } catch (e) {
     res.status(400).send(e);
+  }
+});
+
+// Delete task
+app.delete('/tasks/:id', async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const task = await Task.findByIdAndDelete(_id);
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (e) {
+    res.status(500).send();
   }
 });
 
