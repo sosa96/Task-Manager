d4b451fff846efb02284b3455bdf96114763ce96
Zdravko Sosa <zdravko.sosa@gmail.com>
2021-08-26 20:05:30 +0200

    Added token authentication middleware

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 const router = new express.Router();
 
 // Create user
@@ -29,14 +30,9 @@ router.post('/users/login', async (req, res) => {
   }
 });
 
-// Fetch all users
-router.get('/users', async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (e) {
-    res.status(500).send();
-  }
+// Fetch user profile
+router.get('/users/me', auth, async (req, res) => {
+  res.send(req.user);
 });
 
 // Fetch users by id
