fdd30df34bcc137eb9c24cc72f694f3b82424d2f
Zdravko Sosa <zdravko.sosa@gmail.com>
2021-09-08 17:40:18 +0200

    Added authentification for file uploads

--- src/routers/user.js
+++ src/routers/user.js
@@ -101,7 +101,6 @@ router.delete('/users/me', auth, async (req, res) => {
 
 // Avatar image upload
 const upload = multer({
-  dest: 'avatars',
   limits: {
     fileSize: 1000000, //1mb
   },
@@ -116,13 +115,23 @@ const upload = multer({
 
 router.post(
   '/users/me/avatar',
+  auth,
   upload.single('avatar'),
-  (req, res) => {
+  async (req, res) => {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
     res.send();
   },
   (error, req, res, next) => {
     res.status(400).send({ error: error.message });
   }
 );
+
+// Delete image avatar
+router.delete('/users/me/avatar', auth, async (req, res) => {
+  req.user.avatar = undefined;
+  await req.user.save();
+  res.send();
+});
 
 module.exports = router;
