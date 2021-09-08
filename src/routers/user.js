9dbafa7e70669e8ffc1c23db11e1f9891ef37b53
Zdravko Sosa <zdravko.sosa@gmail.com>
2021-09-08 17:16:07 +0200

    Added file upload validation

--- src/routers/user.js
+++ src/routers/user.js
@@ -102,6 +102,16 @@ router.delete('/users/me', auth, async (req, res) => {
 // Avatar image upload
 const upload = multer({
   dest: 'avatars',
+  limits: {
+    fileSize: 1000000, //1mb
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please upload an image!'));
+    }
+
+    cb(undefined, true);
+  },
 });
 
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
